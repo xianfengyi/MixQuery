a7d22a7425a796663a9be8aa08bcc72874a60f49
yixianfeng <yixianfeng@bytedance.com>
2022-08-31 21:18:25 +0800

    修改类注释

--- jdbc/src/main/java/com/github/pioneeryi/exception/MixqJdbcException.java
+++ jdbc/src/main/java/com/github/pioneeryi/exception/MixqJdbcException.java
@@ -1,11 +1,5 @@
 package com.github.pioneeryi.exception;
 
-/**
- * MixqJdbcException.
- *
- * @Author bytedance
- * @Date 2022/8/30 11:16 PM
- */
 public class MixqJdbcException extends RuntimeException {
     public MixqJdbcException(String message) {
         super(message);
