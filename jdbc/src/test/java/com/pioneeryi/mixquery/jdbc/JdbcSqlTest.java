e0117fd4bf02e80cf5f073f37114e7b593548b4e
yixianfeng <yixianfeng@bytedance.com>
2022-11-13 12:01:38 +0800

    完善测试用例

--- jdbc/src/test/java/com/pioneeryi/mixquery/jdbc/JdbcSqlTest.java
+++ jdbc/src/test/java/com/pioneeryi/mixquery/jdbc/JdbcSqlTest.java
@@ -7,7 +7,7 @@ import java.sql.*;
 public class JdbcSqlTest {
 
     @Test
-    public void testMixSqlQuery_simple() throws ClassNotFoundException {
+    public void testMixQuery_simpleSQL() throws ClassNotFoundException {
         Class.forName("com.pioneeryi.mixquery.jdbc.Driver");
 
         try (Connection conn = DriverManager.getConnection("jdbc:mixquery:localhost:9093/mixquery")) {
@@ -21,7 +21,7 @@ public class JdbcSqlTest {
     }
 
     @Test
-    public void testMixSqlQuery_join() throws ClassNotFoundException {
+    public void testMixQuery_joinSQL() throws ClassNotFoundException {
         Class.forName("com.pioneeryi.mixquery.jdbc.Driver");
 
         try (Connection conn = DriverManager.getConnection("jdbc:mixquery:localhost:9093/mixquery")) {
